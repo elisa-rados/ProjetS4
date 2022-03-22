9df96e1b9e81e3e91d30db1b43a03fb5bb2dd3ad
AbigailBerthe <100685620+AbigailBerthe@users.noreply.github.com>
2022-03-22 14:22:24 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -11,12 +11,66 @@ package ProjetS4;
  * @author Utilisateur
  */
 public class Case {
-    float cote;
-    boolean accessible;   //vrai pour les cases accessibles et faux sinon
-    boolean pion=false;
-    public Case(float a, boolean acc){
-        this.cote=a;
+    private float cote;
+    private boolean accessible;   //vrai pour les cases accessibles et faux sinon
+    private boolean pion= false;
+    private boolean monstre= false;  
+    private int posX;   //position par rapport à l'horizontal
+    private int posY;   //position par rapport à la verticale
+    private boolean blocDePierre = false;
+    
+    public Case(int x, int y, boolean acc){
+        this.posX=x;
+        this.posY=y;
         this.accessible=acc;
     }
     
+    public boolean getBlocDePierre(){
+        return this.blocDePierre;
+    }
+    
+    public void setBlocDePierre(boolean b){
+        this.blocDePierre=b;
+    }
+    
+    public boolean getPion(){
+        return this.pion;
+    }
+    
+    public void setPion(boolean p){
+        this.pion=p;
+    }
+    
+    public boolean getMonstre(){
+        return this.monstre;
+    }
+    
+    public void setMonstre(boolean m){
+        this.monstre=m;
+    }
+    
+     public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public boolean isAccessible() {
+        return accessible;
+    }
+
+    public void setAccessible(boolean accessible) {
+        this.accessible = accessible;
+    }
+
 }
